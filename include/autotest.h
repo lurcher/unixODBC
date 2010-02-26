327d4d51720a7eaa3f97331a748fbd81ca997e6e
Peter Harvey <pharvey@codebydesign.com>
2010-02-26 18:24:03 +0000

    - Reinstate this as autotest.h home and ensure autotest.h gets installed   for dev work. This is to avoid circular dependency between unixODBC-GUI-Qt and unixODBC-Test.
    git-svn-id: svn+ssh://svn.code.sf.net/p/unixodbc/code/trunk@13 ab163d8d-7830-4be3-ba5a-e01213c9a3c2

--- include/autotest.h
+++ include/autotest.h
@@ -1,29 +1,44 @@
-/*---------------------------------------------------------------------------------
- |  Title:		AUTOTEST.H
- |
- |	This file contains constants and prototypes required to compile an 
- |	Auto Test DLL.
- |
- |  Should be consistant with MS version
- |
- *---------------------------------------------------------------------------------*/
-#ifndef AUTOTEST_DEFS
-#define AUTOTEST_DEFS
+/*! 
+ *  \file
+ *          
+ *          This file contains constants and prototypes required to compile an
+ *          Auto Test library/plugin. This is used by the unixODBC-Test and
+ *          unixODBC-GUI-Qt projects.
+ *  
+ *  \note
+ *  
+ *          The contents of this file must be consistent with MS version so as to
+ *          maintain source code portability. This should allow (for example)
+ *          Auto Tests to be compiled for all platforms without source changes.
+ *  
+ */
+#ifndef AUTOTEST_H
+#define AUTOTEST_H
 
+/* standard C stuff... */
 #include <stdlib.h>
 #include <string.h>
-#include "sql.h"
-#include "sqlext.h"
-#include "sqltypes.h"
+
+/* platform specific... */
+#ifdef _WINDOWS
+    #include <windows.h>
+#endif
+
+/* standard ODBC stuff... */
+#include <sql.h>
+#include <sqlext.h>
+#include <sqltypes.h>
 
 #ifdef __cplusplus
-extern "C" { 
-#endif	/* __cplusplus */
+extern "C" {
+#endif
 
-#ifdef PATH_MAX
-#define _MAX_PATH           PATH_MAX
-#else
-#define _MAX_PATH           256
+#ifndef WIN32
+    #ifdef PATH_MAX
+        #define _MAX_PATH           PATH_MAX
+    #else
+        #define _MAX_PATH           256
+    #endif
 #endif
 
 extern  HINSTANCE           hLoadedInst;
@@ -42,8 +57,16 @@ extern  HINSTANCE           hLoadedInst;
 #define MAX_USER_INFO	  				    50
 #define MAX_KEYWORD_LEN	 			        149
 
-#define EXTFUNCDECL
-#define EXTFUN							
+/*  */
+#ifdef WIN32
+    #define EXTFUNCDECL                     _stdcall
+    #define EXTFUN                          _stdcall
+    #define MY_EXPORT                       __declspec(dllexport)
+#else
+    #define EXTFUNCDECL
+    #define EXTFUN							
+    #define MY_EXPORT 
+#endif
 
 #define InitTest(lps)															\
 { 	lps->cErrors=0; }
@@ -66,44 +89,46 @@ extern  HINSTANCE           hLoadedInst;
  * Message box defines
  */
 
-#define MB_OK                               (0x0000)
-#define MB_ABORTRETRYIGNORE                 (0x0001)
-#define MB_OKCANCEL                         (0x0002)
-#define MB_RETRYCANCEL                      (0x0003)
-#define MB_YESNO                            (0x0004)
-#define MB_YESNOCANCEL                      (0x0005)
+#ifndef WIN32
+    #define MB_OK                               (0x0000)
+    #define MB_ABORTRETRYIGNORE                 (0x0001)
+    #define MB_OKCANCEL                         (0x0002)
+    #define MB_RETRYCANCEL                      (0x0003)
+    #define MB_YESNO                            (0x0004)
+    #define MB_YESNOCANCEL                      (0x0005)
+    
+    #define MB_ICONEXCLAMATION                  (0x0000)
+    #define MB_ICONWARNING                      MB_ICONEXCLAMATION
+    #define MB_ICONINFORMATION                  (0x0010)
+    #define MB_ICONASTERISK                     MB_ICONINFORMATION
+    #define MB_ICONQUESTION                     (0x0020)
+    #define MB_ICONSTOP                         (0x0030)
+    #define MB_ICONERROR                        MB_ICONSTOP
+    #define MB_ICONHAND                         MB_ICONSTOP
+    
+    #define MB_DEFBUTTON1                       (0x0000)
+    #define MB_DEFBUTTON2                       (0x0100)
+    #define MB_DEFBUTTON3                       (0x0200)
+    #define MB_DEFBUTTON4                       (0x0300)
+    
+    #define MB_APPMODAL                         (0x0000)
+    #define MB_SYSTEMMODAL                      (0x1000)
+    #define MB_TASKMODAL                        (0x2000)
+    
+    #define MB_DEFAULT_DESKTOP_ONLY             (0x0000)
+    #define MB_HELP                             (0x0000)
+    #define MB_RIGHT                            (0x0000)
+    #define MB_RTLREADING                       (0x0000)
+    #define MB_SETFOREGROUND                    (0x0000)
+    #define MB_TOPMOST                          (0x0000)
+    #define MB_SERVICE_NOTIFICATION             (0x0000)
+    #define MB_SERVICE_NOTIFICATION_NT3X        (0x0000)
+#endif
 
-#define MB_ICONEXCLAMATION                  (0x0000)
-#define MB_ICONWARNING                      MB_ICONEXCLAMATION
-#define MB_ICONINFORMATION                  (0x0010)
-#define MB_ICONASTERISK                     MB_ICONINFORMATION
-#define MB_ICONQUESTION                     (0x0020)
-#define MB_ICONSTOP                         (0x0030)
-#define MB_ICONERROR                        MB_ICONSTOP
-#define MB_ICONHAND                         MB_ICONSTOP
-
-#define MB_DEFBUTTON1                       (0x0000)
-#define MB_DEFBUTTON2                       (0x0100)
-#define MB_DEFBUTTON3                       (0x0200)
-#define MB_DEFBUTTON4                       (0x0300)
-
-#define MB_APPMODAL                         (0x0000)
-#define MB_SYSTEMMODAL                      (0x1000)
-#define MB_TASKMODAL                        (0x2000)
-
-#define MB_DEFAULT_DESKTOP_ONLY             (0x0000)
-#define MB_HELP                             (0x0000)
-#define MB_RIGHT                            (0x0000)
-#define MB_RTLREADING                       (0x0000)
-#define MB_SETFOREGROUND                    (0x0000)
-#define MB_TOPMOST                          (0x0000)
-#define MB_SERVICE_NOTIFICATION             (0x0000)
-#define MB_SERVICE_NOTIFICATION_NT3X        (0x0000)
-
-/*
-// This structure contains the information found in the .INI file for a 
-//	data source.  The filled out structure is in turn passed to AutoTestFunc
-//	to drive the individual tests.
+/*! 
+    This structure contains the information found in the .INI file for a 
+    data source.  The filled out structure is in turn passed to AutoTestFunc
+	to drive the individual tests.
 */
 typedef struct tagSERVERINFO {
 	HWND	 		hwnd;								/* Output edit window */
@@ -138,9 +163,6 @@ typedef struct tagSERVERINFO {
 typedef SERVERINFO FAR * lpSERVERINFO;
 
 
-/*----------------------------------------------------------------------------------
-// Function prototypes
-//---------------------------------------------------------------------------------*/
 BOOL EXTFUNCDECL FAR szLogPrintf(lpSERVERINFO lps, BOOL fForce, LPTSTR szFmt, ...);
 int EXTFUNCDECL FAR szMessageBox(HWND hwnd, UINT style, LPTSTR szTitle, LPTSTR szFmt, ...);
 LPTSTR EXTFUN GetRCString(HINSTANCE hInst, LPTSTR buf, int cbbuf, UINT ids);
